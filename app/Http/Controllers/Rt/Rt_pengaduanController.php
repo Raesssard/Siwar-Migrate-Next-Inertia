84c268259ed898b0bc24c87269c74f8a2783ed00
NeoFelisx <162261010+Raesssard@users.noreply.github.com>
2025-09-12 15:00:45 +0700

    Update Rt_pengaduanController.php

--- app/Http/Controllers/Rt/Rt_pengaduanController.php
+++ app/Http/Controllers/Rt/Rt_pengaduanController.php
@@ -7,44 +7,34 @@ use App\Models\Pengaduan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class Rt_pengaduanController extends Controller
+class Rt_PengaduanController extends Controller
 {
     public function index(Request $request)
     {
-        $title = 'Pengaduan RT Saya';
-        $user = Auth::user();
+        $title = 'Daftar Pengaduan Warga';
 
-        $pengaduan_rt = $user->rukunTetangga->rt;
+        $nik = Auth::user()->nik;
+        $total_pengaduan = Pengaduan::where('nik_warga', $nik)->count();
 
-        $pengaduan_rt_saya = Pengaduan::WhereHas('warga.kartuKeluarga.rukunTetangga', function ($aduan) use ($pengaduan_rt) {
-            $aduan->where('rt', $pengaduan_rt);
-        });
-
-        if ($request->filled('search')) {
-            $hasil = $request->input('search');
-            $pengaduan_rt_saya->where(function ($item) use ($hasil) {
-                $item->where('judul', 'like', "%$hasil%");
-            });
-        }
-
-        $rt_pengaduan = $pengaduan_rt_saya->orderBy('updated_at', 'desc')
+        $pengaduan = Pengaduan::with('warga')
+            ->when($request->filled('search'), function ($q) use ($request) {
+                $search = $request->input('search');
+                $q->where('judul', 'like', "%$search%")
+                  ->orWhereHas('warga', function ($sub) use ($search) {
+                      $sub->where('nama', 'like', "%$search%");
+                  });
+            })
+            ->orderBy('updated_at', 'desc')
             ->paginate(10);
 
-        $total_pengaduan_rt = $rt_pengaduan->count();
-
-        return view('rt.pengaduan.pengaduan', compact('title', 'rt_pengaduan',));
+        return view('rt.pengaduan.pengaduan', compact('title', 'pengaduan', 'total_pengaduan'));
     }
 
-    public function baca($id)
+    public function show($id)
     {
-        $rt_user = Auth::user()->warga->kartuKeluarga->rukunTetangga->rt;
+        $title = 'Detail Pengaduan';
+        $pengaduan = Pengaduan::with('warga')->findOrFail($id);
 
-        $pengaduan = Pengaduan::whereHas('warga.kartuKeluarga.rukunTetangga', function ($q) use ($rt_user) {
-            $q->where('rt', $rt_user);
-        })->where('id', $id)->firstOrFail();
-
-        $pengaduan->update(['status' => 'sudah']);
-
-        return response()->json(['success' => true]);
+        return view('rt.pengaduan.komponen.detail_pengaduan', compact('title', 'pengaduan'));
     }
 }
